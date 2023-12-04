626091123717e19d13e04623861ec97035c374e2
sijo <sijo@accubits.com>
2023-12-04 21:21:26 +0530

    error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import taskRoutes from './routes/tasks';
 
 const app = express();
@@ -9,6 +9,12 @@ app.use('/tasks', taskRoutes); // Add this line to mount the Task API routes
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript Express!');
+});
+
+// Add this error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong');
 });
 
 app.listen(port, () => {
