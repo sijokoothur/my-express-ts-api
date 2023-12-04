76dc0efdaeed118e9cd4afba8a69b91c47c00ae0
sijo <sijo@accubits.com>
2023-12-04 21:19:40 +0530

    input validation

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,15 +1,28 @@
 import { Router, Request, Response } from 'express';
 import { Task } from '../models/task';
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
 let tasks: Task[] = [];
 
-router.post('/', (req: Request, res: Response) => {
+const taskValidationRules = [
+    body('title').notEmpty().withMessage('Title is required'),
+    body('description').notEmpty().withMessage('Description is required'),
+    body('completed').isBoolean().withMessage('Completed must be a boolean'),
+];
+
+router.post('/', taskValidationRules, (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     const task: Task = {
         id: tasks.length + 1,
         title: req.body.title,
         description: req.body.description,
-        completed: false,
+        completed: req.body.completed,
     };
 
     tasks.push(task);
@@ -31,6 +44,12 @@ router.get('/:id', (req: Request, res: Response) => {
 });
 
 router.put('/:id', (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
     const task = tasks.find((t) => t.id === parseInt(req.params.id));
 
     if (!task) {
